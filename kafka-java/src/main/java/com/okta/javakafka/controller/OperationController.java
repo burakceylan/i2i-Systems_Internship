c6b88fbbbd11f70025650f21d4fea962dd21c228
burakceylan <ceylan_burak@outlook.com>
2021-08-07 09:43:38 +0300

    Kafka

---
+++ kafka-java/src/main/java/com/okta/javakafka/controller/OperationController.java
@@ -0,0 +1,22 @@
+package com.okta.javakafka.controller;
+
+public class OperationController {
+    int operand;
+    String operation;
+
+    public int getOperand() {
+        return operand;
+    }
+
+    public void setOperand(int operand) {
+        this.operand = operand;
+    }
+
+    public String getOperation() {
+        return operation;
+    }
+
+    public void setOperation(String operation) {
+        this.operation = operation;
+    }
+}
---
+++ kafka-java/src/main/java/com/okta/javakafka/serializer/KafkaJsonDeserializer.java
@@ -0,0 +1,38 @@
+package com.okta.javakafka.serializer;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.kafka.common.serialization.Deserializer;
+
+import java.util.Map;
+
+public class KafkaJsonDeserializer<T> implements Deserializer {
+
+    private Class<T> type;
+
+    public KafkaJsonDeserializer(Class type) {
+        this.type = type;
+    }
+
+    @Override
+    public void configure(Map map, boolean b) {
+
+    }
+
+    @Override
+    public Object deserialize(String s, byte[] bytes) {
+        ObjectMapper mapper = new ObjectMapper();
+        T obj = null;
+        try {
+            obj = mapper.readValue(bytes, type);
+        } catch (Exception e) {
+
+            //logger.error(e.getMessage());
+        }
+        return obj;
+    }
+
+    @Override
+    public void close() {
+
+    }
+}

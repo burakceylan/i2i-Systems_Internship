10b83edbb6f791ebe98c025bf0433dad303c5284
burakceylan <ceylan_burak@outlook.com>
2021-08-14 22:27:52 +0300

    Swift

--- i2icom/LoginView.swift
+++ i2icom/LoginView.swift
@@ -15,19 +15,27 @@ struct LoginView: View {
             LinearGradient(gradient: Gradient(colors: [Color.white,Color(red: 0/255, green: 183/255, blue: 150/255)]), startPoint: .top, endPoint: .bottom).edgesIgnoringSafeArea(.all)
             
             Group {
-                VStack(spacing:10){
+                VStack(spacing:15){
                     HStack {
                         Text("Phone Number").font(.largeTitle).bold().foregroundColor(Color.black)
                         Spacer()
                     }
                     
-                    TextField(" (5**) *** ** **", text: $number).textFieldStyle(RoundedBorderTextFieldStyle())
+                    TextField("   (5**) *** ** **", text: $number)
+                        .background(RoundedRectangle(cornerRadius: 10).frame(width: 300, height: 50, alignment: .center).foregroundColor(Color.white)).overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color.black, lineWidth: 1).frame(width: 300, height: 50, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                        )
                     
                     HStack {
                         Text("Password").font(.largeTitle).bold().foregroundColor(Color.black)
                         Spacer()
                     }
-                    SecureField("", text: $password).textFieldStyle(RoundedBorderTextFieldStyle())
+                    SecureField("", text: $password)
+                        .background(RoundedRectangle(cornerRadius: 10).frame(width: 300, height: 50, alignment: .center).foregroundColor(Color.white)).overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color.black, lineWidth: 1).frame(width: 300, height: 50, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                        )
                     Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
                         Text("LOGIN").font(.largeTitle).bold().foregroundColor(Color.white)
                     })
